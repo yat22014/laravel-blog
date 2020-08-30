538d325d3373059d22f6a288672ca9175786da9b
Frank <yat22011@gmail.com>
2020-08-31 00:38:06 +0800

    init project

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Comments;
+use App\Http\Controllers\Controller;
+
+class CommentController extends Controller
+{
+    public function store(Request $request)
+    {
+      //on_post, from_user, body
+      $input['from_user'] = $request->user()->id;
+      $input['on_post'] = $request->input('on_post');
+      $input['body'] = $request->input('body');
+      $slug = $request->input('slug');
+      Comments::create( $input );
+      return redirect($slug)->with('message', 'Comment published');     
+    }
+}
